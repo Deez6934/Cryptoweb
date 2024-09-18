03f0bcd5a69c670a6c54e00d44ca3099056a54a8
Akhil <asajanmathew@gmail.com>
2024-09-19 00:20:35 +0530

    Trying another method of creating the foundation.

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from website import config
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = config.api_key
--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
 from flask import Blueprint
+import yfinance as yf
+import pandas as pd
+import mplfinance as mpf
+from io import BytesIO
+import base64
 
 views = Blueprint('views', __name__)
 
